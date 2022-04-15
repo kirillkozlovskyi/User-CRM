eff5c5b0e04ef96d4cef1c04c11d511909c263f3
Kirill Kozlovskyi <kirillkozlovskyi@MacBook-Pro-Kirill.local>
2022-04-15 19:07:13 +0300

    add packages cors, morgan, body-parser

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const authRouter = require('./routes/auth');
 const analyticsRoute = require('./routes/analytics');
 const categoryRoute = require('./routes/category');
 const orderRoute = require('./routes/order');
 const positionRoute = require('./routes/position');
-
 const app = express();
+
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('cors')())
 
 app.use('/api/auth', authRouter);
 app.use('/api/analytics', analyticsRoute);
