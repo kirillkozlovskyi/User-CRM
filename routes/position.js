47bf07723574ba25930669bfd2d2d972189aebf9
kirillkozlovskyi <cucuruza>
2022-04-19 16:51:27 +0300

    add tocken guard

--- routes/position.js
+++ routes/position.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const controllers = require('../controllers/position');
+const passport = require("passport");
 const router = express.Router();
 
-router.get('/:categoryId', controllers.getByCategoryId)
+router.get('/:categoryId', passport.authenticate('jwt', {session: false}), controllers.getByCategoryId)
 
-router.post('/', controllers.create);
+router.post('/', passport.authenticate('jwt', {session: false}), controllers.create);
 
-router.delete('/:id', controllers.remove)
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controllers.remove)
 
-router.patch('/:id', controllers.update);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), controllers.update);
 
 
 module.exports = router;
