9e80ea2373769c2bd0af9bd2d8cacd2278873359
cucuruza <kk@codevery.com>
2022-05-25 16:07:21 +0300

    add site-layout template

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RegisterPageComponent } from './register-page/register-page.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {TokenInterceptor} from "./shared/classes/token.interceptor";
+import { OverviewPageComponent } from './overview-page/overview-page.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import {TokenInterceptor} from "./shared/classes/token.interceptor";
     LoginPageComponent,
     AuthLayoutComponent,
     SiteLayoutComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    OverviewPageComponent
   ],
   imports: [
     BrowserModule,
